a6da9b4c87ca7f7244cc6bf710b51b2dce52497d
AasEsh4 <aashishbasyal4@gmail.com>
2025-09-15 20:21:27 +1200

    Delete .github/workflows/test_bowling.py

--- .github/workflows/test_bowling.py
+++
@@ -1,9 +0,0 @@
-from bowling_game import example_game
-
-def test_example_game_score():
-    game = example_game()
-    assert game.score() == 190
-
-def test_dummy_math():
-    # This is a backup test to prove pytest runs
-    assert 2 + 2 == 4
