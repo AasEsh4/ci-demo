9649b33278883045e1761d3951bae922c0a3a74c
AasEsh4 <aashishbasyal4@gmail.com>
2025-09-15 20:20:53 +1200

    Update test_bowling.py

--- test_bowling.py
+++ test_bowling.py
@@ -1,2 +1,9 @@
-def test_math():
-    assert 2 + 2 == 4
+from bowling_game import example_game
+
+def test_example_game_score():
+    game = example_game()
+    assert game.score() == 190
+
+def test_dummy_math():
+    # simple test to confirm pytest runs
+    assert 1 + 1 == 2
